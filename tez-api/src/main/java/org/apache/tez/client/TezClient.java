87cfffba61b5c676827e90de5b3c3bad9f3b9a3f
Hitesh Shah <hitesh@hortonworks.com>
2016-04-05 16:35:06 -0700

    TEZ-3177. (BUG-47365) Non-DAG events should use the session domain or no domain if the data does not need protection.
    (cherry picked from commit a847e085cef238c2931a7e8320b1b43b44b54699)
    
    Conflicts:
    	HDP-CHANGES.txt

--- tez-api/src/main/java/org/apache/tez/client/TezClient.java
+++ tez-api/src/main/java/org/apache/tez/client/TezClient.java
@@ -119,6 +119,7 @@ public class TezClient {
   private JavaOptsChecker javaOptsChecker = null;
 
   private int preWarmDAGCounter = 0;
+  private int dagCounter = 0;
 
   private static final String atsHistoryLoggingServiceClassName =
       "org.apache.tez.dag.history.logging.ats.ATSHistoryLoggingService";
@@ -430,6 +431,7 @@ public class TezClient {
    *           if submission timed out
    */  
   public synchronized DAGClient submitDAG(DAG dag) throws TezException, IOException {
+    ++dagCounter;
     if (isSession) {
       return submitDAGSession(dag);
     } else {
@@ -471,7 +473,8 @@ public class TezClient {
     if (historyACLPolicyManager != null && sessionHistoryLoggingEnabled) {
       try {
         aclConfigs = historyACLPolicyManager.setupSessionDAGACLs(
-            amConfig.getTezConfiguration(), sessionAppId, dag.getName(), dag.getDagAccessControls());
+            amConfig.getTezConfiguration(), sessionAppId,
+            Integer.toString(dagCounter), dag.getDagAccessControls());
       } catch (HistoryACLPolicyException e) {
         LOG.warn("Disabling history logging for dag " +
           dag.getName() + " due to error in setting up history acls " + e);
