yq12yq/tez-release

commit 181621835a99c9c2716ab095fcd3fb652f8e36f9

TEZ-3224. User payload is not initialized before creating vertex manager plugin. (Zhiyuan Yang via hitesh)
(cherry picked from commit 014d7c4ab4fca8d6119ea6ea993de3632b9f7609)

Conflicts:
	CHANGES.txt

(cherry picked from commit ea23646de6de6656a437ffee3a855abb66093953)

Conflicts:
	CHANGES.txt

tez-dag/src/test/java/org/apache/tez/dag/app/dag/impl/TestVertexManager.java
@@ -19,6 +19,7 @@
 package org.apache.tez.dag.app.dag.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.doReturn;
@@ -27,6 +28,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -38,6 +41,8 @@ import java.util.concurrent.Callable;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.yarn.event.EventHandler;
 import org.apache.tez.dag.api.InputDescriptor;
+import org.apache.tez.dag.api.TezException;
+import org.apache.tez.dag.api.UserPayload;
 import org.apache.tez.dag.api.VertexManagerPlugin;
 import org.apache.tez.dag.api.VertexManagerPluginContext;
 import org.apache.tez.dag.api.VertexManagerPluginDescriptor;
@@ -94,6 +99,35 @@ public class TestVertexManager {
     requestCaptor = ArgumentCaptor.forClass(VertexEventInputDataInformation.class);
 
   }
+
+  public static class CheckUserPayloadVertexManagerPlugin extends VertexManagerPlugin {
+    public CheckUserPayloadVertexManagerPlugin(VertexManagerPluginContext context) {
+      super(context);
+      assertNotNull(context.getUserPayload());
+    }
+
+    @Override
+    public void initialize() throws Exception {}
+
+    @Override
+    public void onVertexManagerEventReceived(VertexManagerEvent vmEvent) throws Exception {}
+
+    @Override
+    public void onRootVertexInitialized(String inputName, InputDescriptor inputDescriptor,
+                                        List<Event> events) throws Exception {}
+  }
+
+  @Test(timeout = 5000)
+  public void testVertexManagerPluginCtorAccessUserPayload() throws IOException, TezException {
+    byte[] randomUserPayload = {1,2,3};
+    UserPayload userPayload = UserPayload.create(ByteBuffer.wrap(randomUserPayload));
+    VertexManager vm =
+        new VertexManager(
+            VertexManagerPluginDescriptor.create(CheckUserPayloadVertexManagerPlugin.class
+                .getName()).setUserPayload(userPayload), UserGroupInformation.getCurrentUser(),
+            mockVertex, mockAppContext, mock(StateChangeNotifier.class));
+  }
+
   
   @Test(timeout = 5000)
   public void testOnRootVertexInitialized() throws Exception {

tez-dag/src/main/java/org/apache/tez/dag/app/dag/impl/VertexManager.java
@@ -377,13 +377,13 @@ public class VertexManager {
     
     pluginContext = new VertexManagerPluginContextImpl();
     Preconditions.checkArgument(pluginDesc != null);
+    payload = pluginDesc.getUserPayload();
+    pluginFailed = new AtomicBoolean(false);
     plugin = ReflectionUtils.createClazzInstance(pluginDesc.getClassName(),
         new Class[] { VertexManagerPluginContext.class }, new Object[] { pluginContext });
-    payload = pluginDesc.getUserPayload();
     execService = appContext.getExecService();
     eventQueue = new LinkedBlockingQueue<VertexManagerEvent>();
     eventInFlight = new AtomicBoolean(false);
-    pluginFailed = new AtomicBoolean(false);
   }
 
   public VertexManagerPlugin getPlugin() {

commit 0d6cffb9a51f4c4cb68f99f915196864868c9c6b

BUG-59744. Fix build issue caused by 181621835a99c9c2716ab095fcd3fb652f8e36f9 for TEZ-3224.

tez-dag/src/test/java/org/apache/tez/dag/app/dag/impl/TestVertexManager.java
@@ -110,11 +110,19 @@ public class TestVertexManager {
     public void initialize() throws Exception {}
 
     @Override
+    public void onVertexStarted(Map<String, List<Integer>> completions) throws Exception {}
+
+    @Override
+    public void onSourceTaskCompleted(String srcVertexName, Integer taskId) throws Exception {}
+
+    @Override
     public void onVertexManagerEventReceived(VertexManagerEvent vmEvent) throws Exception {}
 
     @Override
     public void onRootVertexInitialized(String inputName, InputDescriptor inputDescriptor,
                                         List<Event> events) throws Exception {}
+
+
   }
 
   @Test(timeout = 5000)