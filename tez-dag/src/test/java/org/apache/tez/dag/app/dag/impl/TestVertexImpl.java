0fd7ff81d12dd76b27cc810ea190c75c49ebbd42
Hitesh Shah <hitesh@apache.org>
2016-09-06 11:15:51 -0700

    TEZ-2852. TestVertexImpl fails due to race in AsyncDispatcher. (Zhiyuan Yang via hitesh) (cherry picked from commit cbd4eacb09b6a8ec350723848c397be42ca524c3)
    Conflicts:
    	CHANGES.txt
    	tez-dag/src/test/java/org/apache/tez/dag/app/dag/impl/TestVertexImpl.java
    
    Change-Id: Ibf6f1227034ebbf76008265b2f0614d21a56fa01

--- tez-dag/src/test/java/org/apache/tez/dag/app/dag/impl/TestVertexImpl.java
+++ tez-dag/src/test/java/org/apache/tez/dag/app/dag/impl/TestVertexImpl.java
@@ -47,6 +47,7 @@ import java.util.concurrent.locks.ReentrantLock;
 import com.google.protobuf.ByteString;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.tez.common.DrainDispatcher;
 import org.apache.tez.common.counters.Limits;
 import org.apache.tez.common.counters.TezCounters;
 import org.slf4j.Logger;
@@ -64,7 +65,6 @@ import org.apache.hadoop.yarn.api.records.Container;
 import org.apache.hadoop.yarn.api.records.ContainerId;
 import org.apache.hadoop.yarn.api.records.NodeId;
 import org.apache.hadoop.yarn.api.records.Resource;
-import org.apache.hadoop.yarn.event.DrainDispatcher;
 import org.apache.hadoop.yarn.event.EventHandler;
 import org.apache.hadoop.yarn.util.Clock;
 import org.apache.hadoop.yarn.util.SystemClock;
